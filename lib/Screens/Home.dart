67becaaa03f8793af56e027ee0b93dc1a8a3472b
yousef <yousefyasser@gmail.com>
2024-12-05 19:32:24 +0200

    5/12/2024 last update

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,12 +1,11 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:wellio/Screens/Login.dart';
-import 'package:wellio/Screens/imagemodel.dart';
 import 'package:wellio/Screens/newChatbot.dart';
 import 'package:wellio/Services/Authentication.dart';
-import 'package:wellio/Widgets/GeminiChatBot.dart';
 import 'package:wellio/Widgets/buttom.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class Home extends StatelessWidget {
   final String userName;
@@ -16,130 +15,140 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
       body: Stack(
         children: [
-          // Gradient background
+
           Container(
-            height: double.infinity,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Color(0xff452C63), Color(0xff3D2C8D)],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
+            height: screenSize.height,
+            width: screenSize.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/111_n.jpg'),
+                fit: BoxFit.cover,
               ),
             ),
           ),
-          // Foreground content
-          Padding(
-            padding: const EdgeInsets.all(20.0),
+
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: FittedBox(
+                            fit: BoxFit.scaleDown,
+                            child: Text(
+                              "Welcome, $userName",
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () async {
+                            await AuthServices().signOut();
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (context) => const LoginScreen(),
+                              ),
+                            );
+                          },
+                          icon: const Icon(Icons.exit_to_app, color: Colors.red),
+                          tooltip: 'Logout',
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 50),
+
+
+                    Center(
+                      child: AnimatedTextKit(
+                        animatedTexts: [
+                          TypewriterAnimatedText(
+                            'Welcome to WellioChat,\nWe Are Here to Help You :)',
+                            textAlign: TextAlign.center,
+                            textStyle: const TextStyle(
+                              color: Colors.white70,
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            speed: const Duration(milliseconds: 90),
+                          ),
+                        ],
+                        totalRepeatCount: 2,
+                        pause: const Duration(milliseconds: 5000),
+                        displayFullTextOnTap: true,
+                        stopPauseOnTap: true,
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+
+          Positioned(
+            bottom: 20,
+            left: 16,
+            right: 16,
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // Welcome message and Log Out button
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Welcome, $userName!", // Display the logged-in user's name
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40,
-                        color: Colors.white,
-                      ),
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xffFF4A6E),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
+                CustomButton(
+                  text: "Get Started",
+                  onTap: () async {
+                    String sessionID = DateTime.now().millisecondsSinceEpoch.toString();
+
+                    await FirebaseFirestore.instance
+                        .collection('users')
+                        .doc(userID)
+                        .collection('chats')
+                        .doc(sessionID)
+                        .set({
+                      'createdAt': FieldValue.serverTimestamp(),
+                      'sessionID': sessionID,
+                      'status': 'active',
+                    });
+
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (context) => SkinDiagnosisChatBot(
+                          userName: userName,
+                          userID: userID,
+                          sessionID: sessionID,
                         ),
                       ),
-                      onPressed: () async {
-                        await AuthServices().signOut();
-                        Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(
-                              builder: (context) => const LoginScreen()),
-                        );
-                      },
-                      child: const Text(
-                        "Log Out",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ],
+                    );
+                  },
                 ),
-                const Spacer(),
-                // Animated welcome text
-                Center(
-                  child: AnimatedTextKit(
-                    animatedTexts: [
-                      TypewriterAnimatedText(
-                        'Welcome to WellioChat, \n We Are Here to Help You :) ',
-                        textStyle: const TextStyle(
-                          color: Colors.white70,
-                          fontSize: 32.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        speed: const Duration(milliseconds: 90),
-                      ),
-                    ],
-                    totalRepeatCount: 2,
-                    pause: const Duration(milliseconds: 5000),
-                    displayFullTextOnTap: true,
-                    stopPauseOnTap: true,
-                  ),
-                ),
-                const Spacer(),
-                // Buttons for navigating to different screens
-                Column(
-                  children: [
-                    const SizedBox(height: 20),
-                    CustomButton(
-                      text: "Diagnostic Chatbot",
-                      onTap: () async {
-                        // Generate session ID
-                        String sessionID =
-                            DateTime.now().millisecondsSinceEpoch.toString();
-
-                        // Create the session document
-                        await FirebaseFirestore.instance
-                            .collection('users')
-                            .doc(userID)
-                            .collection('chats')
-                            .doc(sessionID)
-                            .set({
-                          'createdAt': FieldValue.serverTimestamp(),
-                          'sessionID': sessionID,
-                          'status': 'active',
-                        });
-
-                        Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(
-                            builder: (context) => SkinDiagnosisChatBot(
-                                userName: userName,
-                                userID: userID,
-                                sessionID: sessionID),
-                          ),
-                        );
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    CustomButton(
-                      text: "Get Started",
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => Geminichatbot(
-                                userName: userName, userID: userID),
-                          ),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 30),
+                const SizedBox(height: 20),
+                // CustomButton(
+                //   text: "Get Started",
+                //   onTap: () {
+                //     Navigator.of(context).push(
+                //       MaterialPageRoute(
+                //         builder: (context) => Geminichatbot(
+                //           userName: userName,
+                //           userID: userID,
+                //         ),
+                //       ),
+                //     );
+                //   },
+                // ),
               ],
             ),
           ),
--- lib/Widgets/GeminiChatBot.dart
+++ lib/Widgets/GeminiChatBot.dart
@@ -1,5 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:wellio/Screens/Home.dart';
 import 'package:wellio/Widgets/model.dart';
@@ -7,8 +8,7 @@ import 'package:wellio/Widgets/model.dart';
 class Geminichatbot extends StatefulWidget {
   final String userName;
   final String userID;
-  const Geminichatbot(
-      {super.key, required this.userName, required this.userID});
+  const Geminichatbot({super.key, required this.userName, required this.userID});
 
   @override
   State<Geminichatbot> createState() => _GeminichatbotState();
@@ -16,17 +16,16 @@ class Geminichatbot extends StatefulWidget {
 
 class _GeminichatbotState extends State<Geminichatbot> {
   TextEditingController promptController = TextEditingController();
-  static const apiKey = "AIzaSyD_q_DeFBAarzG4uHzHt7kAf2DOti0vmLw";
-  final model = GenerativeModel(model: "gemini-pro", apiKey: apiKey);
+  final ImagePicker _picker = ImagePicker();
+  File? _selectedImage;
 
   final List<ModelMessage> prompt = [];
 
   Future<void> sendMessage() async {
     final message = promptController.text;
-    // for prompt
+
     setState(() {
       promptController.clear();
-
       prompt.add(
         ModelMessage(
           isPrompt: true,
@@ -35,18 +34,30 @@ class _GeminichatbotState extends State<Geminichatbot> {
         ),
       );
     });
-    // for respond
-    final content = [Content.text(message)];
-    final response = await model.generateContent(content);
-    setState(() {
-      prompt.add(
-        ModelMessage(
-          isPrompt: false,
-          message: response.text ?? "",
-          time: DateTime.now(),
-        ),
-      );
-    });
+
+    // Add your chatbot logic or API call here to process the message.
+  }
+
+  Future<void> _pickImage() async {
+    try {
+      final XFile? pickedFile =
+      await _picker.pickImage(source: ImageSource.gallery);
+
+      if (pickedFile != null) {
+        setState(() {
+          _selectedImage = File(pickedFile.path);
+          prompt.add(
+            ModelMessage(
+              isPrompt: true,
+              message: 'Image uploaded.',
+              time: DateTime.now(),
+            ),
+          );
+        });
+      }
+    } catch (e) {
+      print("Error picking image: $e");
+    }
   }
 
   @override
@@ -57,8 +68,6 @@ class _GeminichatbotState extends State<Geminichatbot> {
       decoration: const BoxDecoration(
         gradient: LinearGradient(
           colors: [
-            // Color(0xff1c1c1e),
-            // Color(0xff464649),
             Color(0xff5b457c),
             Color(0xff301998),
           ],
@@ -67,20 +76,13 @@ class _GeminichatbotState extends State<Geminichatbot> {
         ),
       ),
       child: Scaffold(
-        // Ensure the background remains transparent
-        // Ensures gradient remains visible
         backgroundColor: Colors.transparent,
         appBar: AppBar(
           backgroundColor: const Color(0xff5b457c),
-          // Add shadow to AppBar
           shadowColor: Colors.black.withOpacity(0.7),
-          // Adjust the shadow intensity
           elevation: 5,
           leading: IconButton(
-            icon: const Icon(
-              Icons.arrow_back,
-              color: Colors.white,
-            ),
+            icon: const Icon(Icons.arrow_back, color: Colors.white),
             onPressed: () {
               Navigator.push(
                 context,
@@ -103,25 +105,20 @@ class _GeminichatbotState extends State<Geminichatbot> {
         ),
         body: Column(
           children: [
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Expanded(
               child: ListView.builder(
                 itemCount: prompt.length,
-                shrinkWrap:
-                    true, // Ensures the ListView doesn't take unnecessary space
-                physics: BouncingScrollPhysics(), // Adds smooth scrolling
+                shrinkWrap: true,
+                physics: const BouncingScrollPhysics(),
                 itemBuilder: (context, index) {
                   final message = prompt[index];
                   return Align(
                     alignment: message.isPrompt
-                        // Align based on sender
                         ? Alignment.centerRight
                         : Alignment.centerLeft,
                     child: ConstrainedBox(
-                      constraints: BoxConstraints(
-                          // Adjust width
-                          // maxWidth: MediaQuery.of(context).size.width * 0.40,
-                          ),
+                      constraints: const BoxConstraints(),
                       child: UserPrompt(
                         isPrompt: message.isPrompt,
                         message: message.message,
@@ -132,25 +129,32 @@ class _GeminichatbotState extends State<Geminichatbot> {
                 },
               ),
             ),
+            if (_selectedImage != null)
+              Image.file(
+                _selectedImage!,
+                height: 150,
+                width: 150,
+                fit: BoxFit.cover,
+              ),
             Padding(
-              padding: EdgeInsets.all(25),
+              padding: const EdgeInsets.all(25),
               child: Row(
                 children: [
                   Expanded(
                     flex: 20,
                     child: TextField(
                       controller: promptController,
-                      style: TextStyle(color: Colors.white, fontSize: 18),
+                      style: const TextStyle(color: Colors.white, fontSize: 18),
                       decoration: InputDecoration(
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(20),
                         ),
                         hintText: "Ask a question",
-                        hintStyle: TextStyle(color: Colors.white54),
+                        hintStyle: const TextStyle(color: Colors.white54),
                       ),
                     ),
                   ),
-                  SizedBox(width: 10),
+                  const SizedBox(width: 10),
                   CircleAvatar(
                     radius: 29,
                     backgroundColor: const Color(0xF9694AFF),
@@ -164,7 +168,18 @@ class _GeminichatbotState extends State<Geminichatbot> {
                     ),
                   ),
                   const SizedBox(width: 10),
-                  _mediaMessageButton(),
+                  CircleAvatar(
+                    radius: 29,
+                    backgroundColor: const Color(0xF9694AFF),
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.image,
+                        color: Colors.white,
+                        size: 28,
+                      ),
+                      onPressed: _pickImage,
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -180,54 +195,34 @@ class _GeminichatbotState extends State<Geminichatbot> {
     required String date,
   }) {
     return Container(
-      padding: EdgeInsets.all(15),
+      padding: const EdgeInsets.all(15),
       margin: EdgeInsets.symmetric(vertical: 1)
           .copyWith(left: isPrompt ? 80 : 15, right: isPrompt ? 15 : 80),
       decoration: BoxDecoration(
-        color: isPrompt ? Color(0xF9694AFF) : Color(0x882E2E3D),
+        color: isPrompt ? const Color(0xF9694AFF) : const Color(0x882E2E3D),
         borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20),
-          topRight: Radius.circular(20),
-          bottomLeft: isPrompt ? Radius.circular(20) : Radius.zero,
-          bottomRight: isPrompt ? Radius.zero : Radius.circular(20),
+          topLeft: const Radius.circular(20),
+          topRight: const Radius.circular(20),
+          bottomLeft: isPrompt ? const Radius.circular(20) : Radius.zero,
+          bottomRight: isPrompt ? Radius.zero : const Radius.circular(20),
         ),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // for prompt and respond
           Text(
             message,
             style: TextStyle(
               fontWeight: isPrompt ? FontWeight.bold : FontWeight.normal,
               fontSize: 18,
-              color: isPrompt ? Colors.white : Colors.white,
+              color: Colors.white,
             ),
           ),
-
-          // for prompt and respond time
-
-          Text(date,
-              style: TextStyle(
-                fontSize: 13,
-                color: isPrompt ? Colors.white : Colors.white,
-              ))
+          Text(
+            date,
+            style: const TextStyle(fontSize: 13, color: Colors.white),
+          ),
         ],
-      ),
-    );
-  }
-
-  Widget _mediaMessageButton() {
-    return CircleAvatar(
-      radius: 29,
-      backgroundColor: const Color(0xF9694AFF),
-      child: IconButton(
-        icon: const Icon(
-          Icons.image,
-          color: Colors.white,
-          size: 28,
-        ),
-        onPressed: sendMessage,
       ),
     );
   }
